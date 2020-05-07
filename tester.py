c5157e44a49e90b8aa3ef29cec5e878091f6114a
Bill Kusters <wmkusters@me.com>
2020-05-07 14:31:57 -0400

    para joseph

--- tester.py
+++ tester.py
@@ -5,9 +5,14 @@ from matplotlib import pyplot as plt
 def main():
     # resp = fetcher("https://covidtracking.com/api/v1/us/daily.csv")
     df = pd.read_csv("https://covidtracking.com/api/v1/states/daily.csv")
+    print(df.columns)
+    raise SystemError(0)
     ca = df[df.state == "CA"]
     ca = ca[["date", "state", "positive"]]
-    ca.plot(x="date", y="positive")
+    ca["date"] = pd.to_datetime(ca.date, format="%Y%m%d")
+    ca = ca.reset_index(drop=True)
+    # ca = ca.set_index("date")
+    # ca.plot(x="date", y=["positive", tested)
     plt.show()
 
 if __name__ == "__main__":
